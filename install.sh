90106151130d6672a197c0bc48cb6ae00d864425
Chris Kelly <chris.kelly@shopify.com>
2021-08-04 11:22:23 -0400

    Add vim color schemes

--- install.sh
+++ install.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
 FILES=".gitconfig .gitignore .tmux.conf .vimrc .zlogin .zprofile .zpreztorc .zshrc"
+VIM_COLOR_FILES="jellybeans.vim solarized.vim"
 DOTFILE_DIR=$(pwd)
 
 for FILE in $FILES; do
   ln -sf $DOTFILE_DIR/$FILE ~/$FILE
+done
+
+mkdir -p ~/.vim/colors
+
+for FILE in $VIM_COLOR_FILES; do
+  ln -sf $DOTFILE_DIR/$FILE ~/.vim/colors/$FILE
 done
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
