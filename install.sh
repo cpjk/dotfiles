c08296a0188870b35d3e22806e9b47354946c52d
Chris Kelly <chris.kelly@shopify.com>
2021-08-04 11:27:48 -0400

    Add prezto clone

--- install.sh
+++ install.sh
@@ -4,6 +4,8 @@ FILES=".gitconfig .gitignore .tmux.conf .vimrc .zlogin .zprofile .zpreztorc .zsh
 VIM_COLOR_FILES="jellybeans.vim solarized.vim"
 DOTFILE_DIR=$(pwd)
 
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+
 for FILE in $FILES; do
   ln -sf $DOTFILE_DIR/$FILE ~/$FILE
 done
