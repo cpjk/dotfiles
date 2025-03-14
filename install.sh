998200f8768932dd624e50e7b3245e4a79d13e72
Chris Kelly <chris.kelly@shopify.com>
2025-03-14 11:54:13 -0600

    Add ghostty config
    also update install.sh for intellij and ghostty

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-FILES=".gitconfig .gitignore .tmux.conf .vimrc .zlogin .zprofile .zpreztorc .zshrc .inputrc"
+FILES=".gitconfig .gitignore .tmux.conf .vimrc .zlogin .zprofile .zpreztorc .zshrc .inputrc .ideavimrc"
 VIM_COLOR_FILES="jellybeans.vim solarized.vim"
 DOTFILE_DIR=$(pwd)
 
@@ -9,6 +9,8 @@ git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$H
 for FILE in $FILES; do
   ln -sf $DOTFILE_DIR/$FILE ~/$FILE
 done
+
+ln -sf $DOTFILE_DIR/ghostty_config ~/Library/Application\ Support/com.mitchellh.ghostty/config
 
 mkdir -p ~/.vim/colors
 
@@ -21,8 +23,3 @@ mkdir ~/.vim-tmp
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 vim +BundleInstall +qall
-
-if [ $SPIN ]; then
-  sudo apt-get install -o DPkg::Lock::Timeout=600 -y fzy
-  sudo apt-get install -o DPkg::Lock::Timeout=600 -y silversearcher-ag
-fi
